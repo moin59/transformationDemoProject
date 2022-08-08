fdfe99e9cb7e43d3bc9d1b852062006c14ae4bbb
moin1989 <moin.uddin@leanix.net>
2022-08-08 11:00:05 +0200

    initial commit

---
+++ apps/frontend/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'transformation-demo-project-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'frontend';
+}
